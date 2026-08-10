e15c5ac5b6ce11f348f1903685ea5737e47d1f4c
Alessandro Napoli <alessandro.luigi.napoli@gmail.com>
2026-08-10 16:07:56 +0200

    added ffi.rs

---
+++ src/ffi.rs
@@ -0,0 +1,35 @@
+// crate/src/ffi.rs
+
+use core::ffi::{c_char, c_int, c_uint, c_void};
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct MultibootInfo {
+    pub flags: u32,
+    pub mem_upper: u32,
+    pub mem_lower: u32,
+}
+
+/// C function declarations
+extern "C" {
+    // VGA
+    pub fn vga_init();
+    pub fn vga_putchar(c: u8);
+    pub fn vga_write(s: *const c_char);
+    pub fn vga_set_color(fg: u8, bg: u8);
+    pub fn vga_clear();
+
+    // Keyboard
+    pub fn keyboard_init();
+    pub fn keyboard_haschar() -> c_int;
+    pub fn keyboard_getchar() -> u8;
+
+    // Timer
+    pub fn timer_init(frequency: u32);
+    pub fn timer_get_ticks() -> u64;
+
+    // IRQ
+    pub fn irq_init();
+    pub fn irq_enable();
+    pub fn irq_disable();
+}
