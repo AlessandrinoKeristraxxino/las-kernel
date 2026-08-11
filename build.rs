bf2d4ba3a0b4f2b5427983d1bb1d87cecce7d195
Jakck10 <jacoporomani1@gmail.com>
2026-08-11 09:49:02 +0200

    added balls

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 // fn main() {
 //     cc::Build::new()
 //         .file("src/boot.c")
-//         .file("src/vga.c")
-//         .file("src/keyboard.c")
-//         .file("src/timer.c")
-//         .file("src/irq.c")
+//         .file("src/drivers/vga.c")
+//         .file("src/drivers/keyboard.c")
+//         .file("src/drivers/timer.c")
+//         .file("src/drivers/irq.c")
 //         .compile("kernel_c");
     
 //     // Usa il linker script
