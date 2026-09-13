29b5a685d42f6be0e44cfd5f1afe0f0d36a108ad
Jakck10 <jacoporomani1@gmail.com>
2026-09-13 11:51:05 +0200

    v11 -.-

--- src/boot.c
+++ src/boot.c
@@ -1,12 +1,25 @@
 // crate/src/boot.c
 
 #include <stdint.h>
+#include "limine.h"
 
 extern void kernel_main(void);
 
-void kernel_entry(void) {
+// --- Limine protocol requests ---
 
-    extern char _bss_start, _bss_end;  // ← cambia da uint8_t a char
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(3);
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+
+// --- fine Limine requests ---
+
+void kernel_entry(void) {
+    extern char _bss_start, _bss_end;
     for (char *p = &_bss_start; p < &_bss_end; p++) {
         *p = 0;
     }
